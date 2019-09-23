b2bbebcb11d44c6737d7faaaa80d5bb6e12b1b92
Yash Raj <yashrj130@gmail.com>
2019-09-23 18:16:32 +0530

    app controller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsApiService } from './news-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NewsNest';
+
+  mArticles: Array<any> = ["a1", "a2", "a3"];
+  mSources: Array<any> = ["s1", "s2", "s3"];
+
+  constructor(private newsapi: NewsApiService) {
+  }
+
+  ngOnInit() {
+    //load articles
+    // this.newsapi.getArticles().subscribe(data => this.mArticles = data["articles"]);
+
+    //load news sources
+    // this.newsapi.getSources().subscribe(data => this.mSources = data["sources"]);
+
+  }
+
+  searchArticles(source){
+		console.log("selected source is: "+source);
+		// this.newsapi.getArticlesByID(source).subscribe(data => this.mArticles = data['articles']);
+	}
 }
