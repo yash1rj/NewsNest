d6371fb7d99933260703424ca5a4ef44ea5a35ba
Yash Raj <yashrj130@gmail.com>
2019-09-24 16:20:30 +0530

    news api service

--- src/app/news-api.service.ts
+++ src/app/news-api.service.ts
@@ -6,19 +6,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class NewsApiService {
 
-  api_key = 'PUT_YOUR_API_KEY_HERE';
+  api_key = '1251bc941487c14c5780fa561caea92ac990';
 
   constructor(private http: HttpClient) { }
 
   getSources() {
     console.log("Getting News Resources");
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
   }
 
   getArticles() {
     console.log("Getting Articles");
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
   }
 
-  getArticlesById() {
+  getArticlesById(source: String) {
     console.log("Getting Specific Articles");
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
   }
 }
